d320b83716b92964544dd3571d4cfa48a7ce8bbc
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-01-10 17:29:10 +0600

    Caching in redux

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -4,34 +4,35 @@ import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useGetTaskQuery } from "@/redux/api/baseApi";
 import { ITask } from "@/types";
 
-
-
 const Task = () => {
-  const {data,isLoading,isError}= useGetTaskQuery(undefined)
-  console.log({data,isLoading,isError})
-  if(isLoading ){
-    return <p>loading....</p>
+  const { data, isLoading, isError } = useGetTaskQuery(undefined, {
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+    refetchOnReconnect: true,
+  });
+  console.log({ data, isLoading, isError });
+  if (isLoading) {
+    return <p>loading....</p>;
   }
 
   return (
     <div className="max-w-7xl mx-auto mt-20 px-5">
-        <h1>Tasks</h1>
+      <h1>Tasks</h1>
       <div className="flex justify-end items-center gap-10">
         <Tabs defaultValue="all">
-        <TabsList className="grid w-full grid-cols-4">
-        <TabsTrigger value="all" >All</TabsTrigger>
-        <TabsTrigger value="low" >Low</TabsTrigger>
-        <TabsTrigger value="medium" >Medium</TabsTrigger>
-        <TabsTrigger value="high" >High</TabsTrigger>
-      </TabsList>
+          <TabsList className="grid w-full grid-cols-4">
+            <TabsTrigger value="all">All</TabsTrigger>
+            <TabsTrigger value="low">Low</TabsTrigger>
+            <TabsTrigger value="medium">Medium</TabsTrigger>
+            <TabsTrigger value="high">High</TabsTrigger>
+          </TabsList>
         </Tabs>
         <AddTaskModal></AddTaskModal>
       </div>
       <div className="space-y-5 mt-5">
-      {data.tasks.map((task:ITask) => (
+        {data.tasks.map((task: ITask) => (
           <TaskCard task={task} key={task.id}></TaskCard>
         ))}
-      
       </div>
     </div>
   );
